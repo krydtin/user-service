eea7a2defbe7052869f3ef4d38052695bd57ffab
krydtin <localhost.max@gmail.com>
2018-04-06 22:54:05 +0700

    Implement createUser, findUserByName service

--- src/main/java/com/krydtin/user/services/impl/UserServiceImpl.java
+++ src/main/java/com/krydtin/user/services/impl/UserServiceImpl.java
@@ -1,22 +1,39 @@
 package com.krydtin.user.services.impl;
 
 import com.krydtin.user.model.User;
+import com.krydtin.user.repositories.UserRepository;
 import com.krydtin.user.services.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class UserServiceImpl implements UserService{
 
+    private UserRepository userRepository;
+    
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    
     @Override
     public User createUser(User user) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        final User existingUser = userRepository.findByUsername(user.getUsername());
+        if (existingUser != null) {
+            //TODO throw Duplicated
+        }
+        return userRepository.save(user);
     }
 
     @Override
     public User findByUsername(String username) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        final User user = userRepository.findByUsername(username);
+        if (user == null) {
+            //TODO throw DataNotFound
+        }
+        return user;
     }
 
     @Override
